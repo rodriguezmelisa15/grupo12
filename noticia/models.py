a9f730d14f42b93c1c765a4030a9d3daee90c6d4
rodriguezmelisa15 <109873727+rodriguezmelisa15@users.noreply.github.com>
2022-08-02 01:49:19 -0300

    inicio de blog

--- noticia/models.py
+++ noticia/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class Noticia(models.Model):
+    titulo = models.CharField(max_length = 255)
+    autor = models.ForeignKey(User, on_delete = models.CASCADE)
+    cuerpo = models.TextField()
+
+    def __str__(self):
+        return self.titulo +' | '+ str(self.autor)
+
