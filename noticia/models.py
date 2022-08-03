3bcfb1b0b34640f840a591920d5914b3a7ffc7f2
Marcos316424 <marcox316424@gmail.com>
2022-08-03 16:49:07 -0300

    Prueba

--- noticia/models.py
+++ noticia/models.py
@@ -5,6 +5,11 @@ class Noticia(models.Model):
     titulo = models.CharField(max_length = 255)
     autor = models.ForeignKey(User, on_delete = models.CASCADE)
     cuerpo = models.TextField()
+<<<<<<< Updated upstream:noticia/models.py
+=======
+    
+    
+>>>>>>> Stashed changes:Grupo-12/apps/Noticia/models.py
 
     def __str__(self):
         return self.titulo +' | '+ str(self.autor)
