34a34a72fa199b885c577187813aba50f7b7ffd2
Marcos Eduardo Eguiazabal <98111090+Marcos316424@users.noreply.github.com>
2022-08-17 18:15:53 -0300

    Restore

--- Noticia/forms.py
+++ Noticia/forms.py
@@ -12,3 +12,19 @@ class NoticiasForm(forms.ModelForm):
             'autor': forms.Select(attrs={'class': 'form-control','placeholder': 'Nombre del autor'}),
             'cuerpo': forms.Textarea(attrs={'class': 'form-control', 'placerholder':'cuerpo de la noticia'}),
         }
+<<<<<<< Updated upstream
+=======
+
+class CrearComentario(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('usuario', 'comentario')
+
+        widgets = {
+            'usuario': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Usuario'}),
+            'titulo_cartel': forms.TextInput(attrs={'class': 'form-control'}),
+            'autor': forms.Select(attrs={'class': 'form-control','placeholder': 'Nombre del autor'}),
+            'comentario': forms.Textarea(attrs={'class': 'form-control', 'placerholder':'comentario'}),
+        }
+        
+>>>>>>> Stashed changes
