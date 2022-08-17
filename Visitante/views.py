a3e274670eba9cea5df8e092bf397850ac77d672
Marcos Eduardo Eguiazabal <98111090+Marcos316424@users.noreply.github.com>
2022-08-17 20:17:34 -0300

    1233

--- Visitante/views.py
+++ Visitante/views.py
@@ -3,9 +3,8 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
+
 class VistaRegistroVisitante(generic.CreateView):
     form_class= UserCreationForm
     template_name = 'registro/registro.html'
     success_url = reverse_lazy('login')
-
-
