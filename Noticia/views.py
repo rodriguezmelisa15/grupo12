4d000c50ad74e900c9680eff7e972bac15cafda4
Marcos Eduardo Eguiazabal <98111090+Marcos316424@users.noreply.github.com>
2022-08-20 00:34:02 -0300

    Registro Completo pero problemas con url de navs

--- Noticia/views.py
+++ Noticia/views.py
@@ -7,35 +7,35 @@ from django.urls import reverse_lazy
 
 class HomeView(ListView):
     model = Noticia
-    template_name = 'home.html'
+    template_name = 'Noticia/home.html'
     ordering = ['-fecha_noticia']
 
 
 class VistaDetalleNoticia(DetailView):
     model = Noticia
-    template_name = 'detalles_noticia.html'
+    template_name = 'Noticia/detalles_noticia.html'
 
 
 class VistaNuevaNoticia(CreateView):
     model = Noticia
     form_class = NoticiasForm
-    template_name = 'crear_noticia.html'
+    template_name = 'Noticia/crear_noticia.html'
     #fields = '__all__'
 
 
 class VistaEliminarNoticia(DeleteView):
     model = Noticia
-    template_name = 'eliminar_noticia.html'
+    template_name = 'Noticia/eliminar_noticia.html'
     success_url = reverse_lazy('home')
 
 
 def VistaQuienesSomos(redirect):
-    return render(redirect, 'nosotros.html')
+    return render(redirect, 'Noticia/nosotros.html')
 
 class ComentarioNoticia(CreateView):
     model = Comment
     form_class = CrearComentario
-    template_name = 'detalles_noticia.html'
+    template_name = 'Noticia/detalles_noticia.html'
     #fields = '__all__'
     """def form_valid(self, form):
         form.instance.noticia_id = self.kwargs['pk']
--- grupo12/settings/base.py
+++ grupo12/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from tempfile import template
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'grupo12.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
