058f6c0d75e1bfa6eccbb3a2b57e379c0e96a1e2
Marcos Eduardo Eguiazabal <98111090+Marcos316424@users.noreply.github.com>
2022-08-20 21:55:48 -0300

    Categorias Noticia

--- Noticia/forms.py
+++ Noticia/forms.py
@@ -4,11 +4,11 @@ from .models import Noticia, Comment
 class NoticiasForm(forms.ModelForm):
     class Meta:
         model = Noticia
-        fields = ('titulo', 'titulo_cartel', 'autor', 'cuerpo')
+        fields = ('titulo', 'categoria', 'autor', 'cuerpo')
 
         widgets = {
             'titulo': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Inserte un titulo a la noticia'}),
-            'titulo_cartel': forms.TextInput(attrs={'class': 'form-control'}),
+            'categoria': forms.Select(attrs={'class': 'form-control'}),
             'autor': forms.Select(attrs={'class': 'form-control','placeholder': 'Nombre del autor'}),
             'cuerpo': forms.Textarea(attrs={'class': 'form-control', 'placerholder':'cuerpo de la noticia'}),
         }
--- Noticia/models.py
+++ Noticia/models.py
@@ -3,10 +3,18 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
-
+Noticia_categoria=[
+	(1,'Cursos'),
+	(2,'Informacion'),
+	(3,'Ingresos'),
+]
 class Noticia(models.Model):
     titulo = models.CharField(max_length=255)
-    titulo_cartel = models.CharField(max_length=255)
+    categoria = models.IntegerField(
+	    null=False, blank=False,
+	    choices= Noticia_categoria,
+	    default=1
+    )
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     cuerpo = models.TextField()
     fecha_noticia = models.DateField(auto_now_add=True)
