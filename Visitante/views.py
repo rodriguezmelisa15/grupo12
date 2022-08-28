d3328df0f8cbcf2db8351ea1c1fb3a13448a7853
federicofogar <110346147+federicofogar@users.noreply.github.com>
2022-08-27 22:00:14 -0300

    Igual que main para video

--- Visitante/views.py
+++ Visitante/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+from django.shortcuts import render,redirect
+from .forms import CustomUserForm
+from django.contrib.auth import login, authenticate
 
-class VistaRegistroVisitante(generic.CreateView):
+"""class VistaRegistroVisitante(generic.CreateView):
     form_class= UserCreationForm
-    template_name = 'registro/registro.html'
+    template_name = 'Visitante/registro/registro.html'
     success_url = reverse_lazy('login')
+"""
+"""
+def Create(request):
+    usuario = request.POST['txtNickname']
+    nombre = request.POST['txtNombre']
+    apellido = request.POST['txtApellido']
+    mail = request.POST['txtMail']
+    contraseña = request.POST['txtContra']
+    Usuario.objects.create(usuario= usuario, nombre= nombre, apellido= apellido, mail=mail, contraseña=contraseña,)
+    return redirect('login/')
+"""
+
+def registro_usuario(request):
+    data = {
+        'form': CustomUserForm()
+    }
+    if request.method == 'POST':
+        formulario = CustomUserForm (request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            username = formulario.cleaned_data['username']
+            password = formulario.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect(to='home')
+    
 
 
+    return render(request,'Visitante/registro.html', data)
